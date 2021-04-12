983ba42e204dce4f0fefa3334eea1a60eb696078
MARTIANSID <59787009+MARTIANSID@users.noreply.github.com>
2021-04-12 14:05:04 +0530

    Create Solution.java

---
+++ GeeksForGeeks/Shortest Common Supersequence/formapalindrome/Solution.java
@@ -0,0 +1,30 @@
+
+class Solution{
+    static int countMin(String str)
+    {
+        // code here
+        
+        int[][] dp=new int[str.length()][str.length()];
+        for(int[] row:dp)Arrays.fill(row,-1);
+        
+        return str.length()-solve(str,0,str.length()-1,dp);
+    }
+    public static int solve(String s,int i,int j,int[][] dp){
+        
+        if(i==j){
+            return dp[i][j]=1;
+        }
+        
+        if(dp[i][j]!=-1)return dp[i][j];
+        
+        if(j<i)return 0;
+        
+        if(s.charAt(i)==s.charAt(j)){
+        return    dp[i][j]=solve(s,i+1,j-1,dp)+2;
+        }else{
+            
+            return dp[i][j]= Math.max(solve(s,i+1,j,dp),solve(s,i,j-1,dp));
+        }
+        
+    }
+}
