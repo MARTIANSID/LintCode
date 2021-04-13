6d802b3ca3a092facc0d6ca9b3fd485b4a386a2c
MARTIANSID <59787009+MARTIANSID@users.noreply.github.com>
2021-04-13 13:49:37 +0530

    Create Solution.java

---
+++ GeeksForGeeks/Shortest Common Supersequence/BuyingVegetables/Solution.java
@@ -0,0 +1,28 @@
+class Solution{
+    static int minCost(int N, int cost[][]) {
+        
+        int[][] dp=new int[cost.length+1][5];
+        for(int[] row:dp)Arrays.fill(row,-1);
+        return solve(N,cost,4,0,dp);
+    }
+    
+    public static int solve(int N,int cost[][],int adj,int l,int[][] dp){
+        
+        if(l==N){
+            return dp[l][adj]=0;
+        }
+        
+        if(dp[l][adj]!=-1)return dp[l][adj];
+        
+        int min=(int)1e9;
+        for(int i=0;i<3;i++){
+            if(i!=adj){
+               min=Math.min(min, solve(N,cost,i,l+1,dp)+cost[l][i]);
+                
+            }
+        }
+        
+        return dp[l][adj]=min;
+        
+    }
+}
