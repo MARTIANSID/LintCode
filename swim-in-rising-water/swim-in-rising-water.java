cc685b2c407900f6f8b65e08fe10b214208ea248
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2021-07-01 20:08:53 +0530

    Time: 12 ms (43.93%), Space: 39 MB (63.69%) - LeetHub

--- swim-in-rising-water/swim-in-rising-water.java
+++ swim-in-rising-water/swim-in-rising-water.java
@@ -1,49 +1,48 @@
 class Solution {
-        
-        //binary search+dfs
     public int swimInWater(int[][] grid) {
-        
-           int si=0,ei=(int)1e9;
-           int n=grid.length;
-           int m=grid[0].length;
-       
-           
-            while(si<=ei){
-                    int mid=(si+ei)/2;
-                     boolean[][] visited=new boolean[n][m];
-                    if(dfs(grid,0,0,n,m,mid,visited)){
-                            
-                            ei=mid-1;
-                    }else{
-                           si=mid+1;
-                    }
-            }
+      
             
+            int n=grid.length;
             
-            return si;
-           
+              PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->{
+                int r1=a/n,c1=a%n;
+                int r2=b/n,c2=b%n;
+                return grid[r1][c1]-grid[r2][c2];      
+        });
+            int[][] dir = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+            boolean[][] visited=new boolean[n][n];
+            int time=0;
+            int minHeight=0;
+            visited[0][0]=true;
+            pq.add(0);
+       while(pq.size()!=0) {
+               int size=pq.size();
+               while(size-->0){
+                       int point=pq.poll();
+                       int r =point/n;
+                       int c=point%n;
+                       
+                       
+                     minHeight=Math.max(minHeight,grid[r][c]);
+                       
+                       
+                        if(r==n-1&&c==n-1)return minHeight;
+                       
+                       
+                      for(int i=0;i<dir.length;i++){
+                              int r1=r+dir[i][0];
+                              int c1=c+dir[i][1];
+                              
+                          if(r1>=0&&c1>=0&&r1<n&&c1<n&&visited[r1][c1]!=true){
+                                  
+                                  pq.add(r1*n+c1);
+                                  visited[r1][c1]=true;
+                          }    
+                              
+                      }
+                      
+               }
+       }
+            return minHeight;
     }
-        
-        public boolean dfs(int[][] grid,int r,int c,int n,int m,int time,boolean[][] visited){
-                
-             if(r>=n||c>=m||r<0||c<0||visited[r][c]==true||grid[r][c]>time){
-                     return false;
-             }
-                
-               
-             if(r==n-1&&c==m-1)return true;  
-                visited[r][c]=true;
-                
-         return  dfs(grid,r+1,c,m,n,time,visited)||dfs(grid,r,c+1,m,n,time,visited)||dfs(grid,r-1,c,m,n,time,visited)||dfs(grid,r,c-1,m,n,time,visited);
-             
-                
-        
-                
-                
-                
-                                
-                
-                
-                
-        }
 }
