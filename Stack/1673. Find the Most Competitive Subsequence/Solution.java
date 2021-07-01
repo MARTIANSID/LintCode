8dac20b07703b5524bee4e3dc7dd8ca28eab14db
MARTIANSID <59787009+MARTIANSID@users.noreply.github.com>
2021-07-01 11:55:39 +0530

    basic calculator short and concise code

--- Stack/1673. Find the Most Competitive Subsequence/Solution.java
+++ Stack/1673. Find the Most Competitive Subsequence/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 class Solution {
     public int[] mostCompetitive(int[] nums, int k) {
      
--- Stack/basic-calculator/basic-calculator.java
+++ Stack/basic-calculator/basic-calculator.java
@@ -1,214 +1,73 @@
 class Solution {
-    public int calculate(String s) {
-       
-        return solve(s,0,s.length()-1);
-    }
         
-        public int solve(String s,int si,int ei){
-                          
-
-           Stack<Integer> st=new Stack<>();
-            int no=0;
-                char sign='0';
-           for(int i=si;i<=ei;i++){
-                 
-        if(Character.isDigit(s.charAt(i))){
-              
-                      no = no*10+s.charAt(i)-'0';
-                
-                if(i==ei){
-                        
-                        
-                        
-                         if(sign=='+'){
-                        
-                        int n=no;
-                        st.push(n);
-                        sign=s.charAt(i);
-                        no=0;
-                        
-                }else if(sign=='-'){
-                       int n=no;
-                        st.push(-n);
-                        sign=s.charAt(i);
-                                               no=0;
-
-                        
-                        
-                }else if(sign=='*'){
-                        
-                        int n=no;
-                        int y=st.pop();
-                        
-                        st.push(n*y);
-                        
-                        
-                        sign=s.charAt(i);
-                                             no=0;
-
-                        
-                        
+        private void solve(Stack<Integer> st,char sign,int no){
+                  if(sign=='+'){
+                                 st.push(no);
+                              
+                                 
+                         }else if(sign=='-'){
+                                 st.push(-no);
+                                 
+                                 
+                         }else if(sign=='*'){
+                                 int number=st.pop();
+                                 int result=number*no;
+                                 st.push(result);
+                             
+                                 
+                         }else if(sign=='/'){
+                                  int number=st.pop();
+                                 int result=number/no;
+                                 st.push(result);
                                 
-                }else if(sign=='/'){
-                        
-                        int n=no;
-                        int y=st.pop();
-                        
-                        st.push(y/n);
-                        
-                        
-                        sign=s.charAt(i);
-                                                no=0;
-
-                        
-                }else {
-                        sign=s.charAt(i);
-                        int n=no;
-                        st.push(n);
-                                            no=0;
-
-                } 
-                        
-                        
-                }
-              }
-        else if(i==ei||s.charAt(i)=='-'||s.charAt(i)=='+'||s.charAt(i)=='*'||s.charAt(i)=='/'||s.charAt(i)=='?'){
-                
-                if(sign=='+'){
-                        
-                        int n=no;
-                        st.push(n);
-                        sign=s.charAt(i);
-                        no=0;
-                        
-                }else if(sign=='-'){
-                       int n=no;
-                        st.push(-n);
-                        sign=s.charAt(i);
-                                               no=0;
-
-                        
-                        
-                }else if(sign=='*'){
-                        
-                        int n=no;
-                        int y=st.pop();
-                        
-                        st.push(n*y);
-                        
-                        
-                        sign=s.charAt(i);
-                                             no=0;
-
-                        
-                        
+                                 
+                         }else{
                                 
-                }else if(sign=='/'){
-                        
-                        int n=no;
-                        int y=st.pop();
-                        
-                        st.push(y/n);
-                        
-                        
-                        sign=s.charAt(i);
-                                                no=0;
-
-                        
-                }else {
-                        sign=s.charAt(i);
-                        int n=no;
-                        st.push(n);
-                                            no=0;
-
-                }
-                
-                
-                
-        }else if(s.charAt(i)=='('){
-                
-                Stack<Character> fuck=new Stack<>();
-                
-                fuck .push('(');
-                
-                i++;
-                int u=i;
-                while(fuck.size()!=0){
-                        if(fuck.peek()=='('&&s.charAt(i)==')'){
-                                fuck.pop();
-                        }else if(s.charAt(i)=='('){
-                                fuck.push('(');
-                        }
-                        i++;
-                }
-                
-                
-            no=solve(s,u,i-2);
-                i--;
-                
-               if(i==ei){
-                         if(sign=='+'){
-                        
-                        int n=no;
-                        st.push(n);
-                        sign=s.charAt(i);
-                        no=0;
-                        
-                }else if(sign=='-'){
-                       int n=no;
-                        st.push(-n);
-                        sign=s.charAt(i);
-                                               no=0;
-
-                        
-                        
-                }else if(sign=='*'){
-                        
-                        int n=no;
-                        int y=st.pop();
-                        
-                        st.push(n*y);
-                        
-                        
-                        sign=s.charAt(i);
-                                             no=0;
-
-                        
-                        
-                                
-                }else if(sign=='/'){
-                        
-                        int n=no;
-                        int y=st.pop();
-                        
-                        st.push(y/n);
-                        
-                        
-                        sign=s.charAt(i);
-                                                no=0;
-
-                        
-                }else {
-                        sign=s.charAt(i);
-                        int n=no;
-                        st.push(n);
-                                            no=0;
-
-                }
-               }
-                       
-              
-           
+                                 st.push(no);
+                               
+                         }
                 
         }
+        
+    public int calculate(String s) {
+        Stack<Integer> st=new Stack<>();
+        int si=0,ei=s.length();
+            int no=0;
+            char sign='~';
+         for(int i=si;i<=ei;i++){
+                
+                 
+                 char ch='~';
+                 if(i!=ei)
+                  ch=s.charAt(i);
+                 
+               
+                 
+                 if(Character.isDigit(ch)){
+                         no=no*10+ch-'0';
+                        
+                        
+                 }else if(ch!=' '&&i!=ei){
+                         solve(st,sign,no);
+                         sign=ch;
+                         no=0;
+                       
+                 }
+                 
+                 if(i==ei){
+                         solve(st,sign,no);
+                         sign=ch;
+                         no=0;
+                 }
+                 
+                 
+                 
+         }
+            int ans=0;
+           for(int i=0;i<st.size();i++){
+                   ans+=st.get(i);
+           }
+            return ans;
             
     }
-            int ans=0;
-            while(st.size()!=0){
-                    int x=st.pop();
-                   // System.out.println(x);
-                    ans+=x;
-            }
-            return ans;
-        }
 }
