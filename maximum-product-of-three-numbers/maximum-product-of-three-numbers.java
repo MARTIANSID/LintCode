ab1cfda6af84e9e161fa2f0666bc4f98490d7214
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2021-07-06 14:30:06 +0530

    Update maximum-product-of-three-numbers.java

--- maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -1,6 +1,7 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-     
+      //we can find first max second max ,third max similarly for min for time complexity o(n)
+            
            Arrays.sort(nums);
             
           return Math.max(nums[0]*nums[1]*nums[nums.length-1],nums[nums.length-3]*nums[nums.length-2]*nums[nums.length-1]);
