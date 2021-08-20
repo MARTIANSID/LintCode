bc12a7bcd3ec90cf72fb8b29bbdb9f7027009463
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2021-08-20 15:26:05 +0530

    Time: 1 ms (80.94%), Space: 36.8 MB (39.26%) - LeetHub

--- video-stitching/video-stitching.java
+++ video-stitching/video-stitching.java
@@ -3,24 +3,20 @@ class Solution {
         Arrays.sort(clips,(a,b)->{
             return a[0]-b[0];
         });
+     
         
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->{
-            return b[1]-a[1];
-        });
-        
-        
-        int start=0,i=0,clip=0;
+        int start=0,i=0,clip=0,max=0;
         
         while(start<time){
             while(i<clips.length&&clips[i][0]<=start){
-                pq.add(clips[i]);
+               max=Math.max(max,clips[i][1]);
                 i++;
             }
             
-            if(pq.size()==0)return -1;
+            if(max<=start)return -1;
             clip++;
             
-            start=pq.poll()[1];
+            start=max;
             
             
         }
