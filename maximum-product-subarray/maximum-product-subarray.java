9fba8cb337b5d4be9e1a1e9c41955ffae65c6572
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2021-08-26 22:35:26 +0530

    optimized approach of maximum product sub

--- maximum-product-subarray/maximum-product-subarray.java
+++ maximum-product-subarray/maximum-product-subarray.java
@@ -1,4 +1,6 @@
 class Solution {
+
+    //o(n) tc ,  // o(n) space 
     public int maxProduct(int[] nums) {
    int[] pre=new int[nums.length];
       int count=0;
@@ -88,4 +90,29 @@ class Solution {
         return mp;
         
     }
+    //o(n) , o(1) space
+   
+        public int mp(int[] nums) {
+            
+            int max=-(int)1e9;
+            int maxEndingAti=1;
+            int minEndingAti=1;
+    
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]==0){
+                    max=Math.max(0,max);
+                    maxEndingAti=1;
+                    minEndingAti=1;
+                }
+                int mx=maxEndingAti;
+                int mi=minEndingAti;
+                maxEndingAti=Math.max(nums[i],Math.max(nums[i]*mx,nums[i]*mi));
+                minEndingAti=Math.min(nums[i],Math.min(nums[i]*mx,nums[i]*mi));
+                max=Math.max(max,maxEndingAti);
+            }
+    
+            return max;
+        }
+    
+
 }
