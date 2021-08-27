82bfb56fa5c38f3605a1efe1cbb6c8941520b6a9
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2021-08-27 12:38:26 +0530

    Time: 4 ms (68.92%), Space: 54.1 MB (7.03%) - LeetHub

--- minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
+++ minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
@@ -1,60 +1,36 @@
 class Solution {
-    class pair{
-        int col;
-        int val;
-        pair(int col,int val){
-            this.col=col;
-            this.val=val;
-        }
-    }
-    
-    Integer[][]  dp;
-    
     public int minFallingPathSum(int[][] grid) {
-        dp=new Integer[grid.length+1][grid[0].length+1];
-        List<List<pair>> mins=new ArrayList<>();
-         
-        for(int i=0;i<grid.length;i++){
-           
-            int min1=(int)1e9,min2=(int)1e9,c1=0,c2=0;
-            List<pair> arr=new ArrayList<>(); 
-            for(int j=0;j<grid[i].length;j++){
-                if(grid[i][j]<=min1){
-                    
+        int n=grid.length;
+        int m=grid[0].length;
+        for(int i=1;i<n;i++){
+            int min1=(int)1e9,min2=(int)1e9,c1=-1,c2=-1;
+            for(int j=0;j<m;j++){
+                if(grid[i-1][j]<=min1){
                     min2=min1;
                     c2=c1;
-                    min1=grid[i][j];
+                    min1=grid[i-1][j];
                     c1=j;
-                }else if(grid[i][j]<min2){
-                    min2=grid[i][j];
+                }else if(grid[i-1][j]<min2){
+                    min2=grid[i-1][j];
                     c2=j;
                 }
             }
-            arr.add(new pair(c1,min1));
-            arr.add(new pair(c2,min2));
-            mins.add(arr);
-        }
-        
-        int min=(int)1e9;
-       for(int i=0;i<2;i++){
-           int col=mins.get(0).get(i).col;
-           min=Math.min(min,solve(mins,1,col)+mins.get(0).get(i).val);
-       }
-        return min;
-    }
-    
-    public int solve(List<List<pair>> mins,int r,int c){
-        if(r==mins.size())return 0;
-        
-        if(dp[r][c]!=null)return dp[r][c];
-        
-        int min=(int)1e9;        
-            for(int i=0;i<2;i++){
-               int col= mins.get(r).get(i).col;
-                if(c!=col){
-                   min=Math.min(min, solve(mins,r+1,col)+mins.get(r).get(i).val);
+            
+            for(int j=0;j<m;j++){
+                if(j!=c1){
+                    grid[i][j]+=min1;
+                }else if(j!=c2){
+                    grid[i][j]+=min2;
                 }
             }
-        return  dp[r][c]= min;
+        }
+        
+        int om=(int)1e9;
+        for(int i=0;i<m;i++){
+            om=Math.min(om,grid[n-1][i]);
+        }
+        
+        return om;
     }
+    
 }
