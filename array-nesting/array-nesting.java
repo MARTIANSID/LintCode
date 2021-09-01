fb13f99cac74601ff569d2177ab1ddf0ebd36bbc
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2021-09-01 16:57:23 +0530

    Update array-nesting.java

--- array-nesting/array-nesting.java
+++ array-nesting/array-nesting.java
@@ -2,21 +2,23 @@ class Solution {
     public int arrayNesting(int[] nums) {
             int n=nums.length;  
         
-        HashSet<Integer> set=new HashSet<>();
+       
         
-        boolean[] visited=new boolean[n];
+       
         int max=0;
         for(int i=0;i<n;i++){
                int val=nums[i];
-            if(visited[val])continue;
+            if(val==-1)continue;
             int count=0;
             while(true){
                 
-                if(set.contains(val))break;
-                set.add(val);
-                visited[val]=true;
+                if(nums[val]==-1)break;
+               
+               
                 
-                val=nums[val];
+                int x=nums[val];
+                 nums[val]=-1;
+                val=x;
                 count++;
             }
             max=Math.max(max ,count);
