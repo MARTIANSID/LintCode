20a1ffb33b3294f365a0457c36bee105625e2731
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-01-09 15:22:14 +0530

    Time: 189 ms (66.67%), Space: 63 MB (16.67%) - LeetHub

--- 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.java
+++ 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.java
@@ -1,49 +1,6 @@
 class Solution {
     
-//     class Trie {
-    
-//     class TrieNode{
-//         TrieNode[] children=new TrieNode[26];
-//         boolean end=false;
-    
-//     } 
-//         TrieNode root;
-//         public Trie(){
-//             root=new TrieNode();
-//         }
-        
-//          public void insert(String word){
-//             TrieNode node=this.root;
-//             for(int i=0;i<word.length();i++){
-//                 char c=word.charAt(i);
-//                 if(node.children[c-'a']==null){
-//                     node.children[c-'a']=new TrieNode();
-//                 }
-//                 node=node.children[c-'a'];
-    
-//             }
-//             node.end=true;
-//         }
-        
-//         public boolean startsWith(String word){
-//             TrieNode node=root;
-//             int[] freq=new int[26];
-//             for(int i=0;i<word.length();i++){
-               
-//                 char c=word.charAt(i);
-//                  freq[c-'a']++;
-//                 if(node.children[c-'a']==null){
-//                     return false;
-//                 }
-//                 node=node.children[c-'a'];
-                
-//             }
-           
-            
-//                 return node.end;
-            
-//         }
-//     }
+
     
     public int wordCount(String[] startWords, String[] targetWords) {
         int ans=0;
@@ -54,11 +11,9 @@ class Solution {
         
         
         
-            for(String t:targetWords){
+            for(String s:startWords){
                 
-           
-        char[] chars = t.toCharArray();
-                
+        char[] chars = s.toCharArray();  
         Arrays.sort(chars);
         String sorted = new String(chars);
             
@@ -68,22 +23,19 @@ class Solution {
         
         
         
-        for(String s:startWords){
-            String temp=s;
-            //   char[] chars = temp.toCharArray();
-            // Arrays.sort(chars);
-            // String sorted = new String(chars);
-            
-            for(int i=0;i<26;i++){
-                String h=temp+(char)(i+'a');
-            char[] cha = h.toCharArray();
-            Arrays.sort(cha);
-            String st = new String(cha);
-                if(map.containsKey(st)){
-                    ans+=map.get(st);
-                    map.remove(st);
+        for(String t:targetWords){
+           
+            for(int i=0;i<t.length();i++){
+                String str = t.substring(0, i) + t.substring(i+1);
+                char[] chars = str.toCharArray();  
+                Arrays.sort(chars);
+                String sorted = new String(chars);
+                if(map.containsKey(sorted)){
+                    ans++;
+                    break;
                 }
             }
+
              
         }
         return ans;
