aef9f78573f5ba1737d468b981874ab60d76909b
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-01-18 12:11:38 +0530

    Time: 3 ms (16.62%), Space: 40.6 MB (29.26%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,27 +1,53 @@
 class Solution {
-    Integer[] dp;
+    // Integer[] dp;
     public int minCostClimbingStairs(int[] cost) {
         int n=cost.length;
         
-        dp=new Integer[n];
         
-        solve(0,cost,n);
+        if(n==1||n==0)return n==1?cost[0]:0; 
         
-        return Math.min(dp[0],dp[1]);
+        int[] dp=new int[n];
+        dp[n-1]=cost[n-1];
+        dp[n-2]=cost[n-2];
+        
+        if(n==2){
+            return Math.min(dp[0],dp[1]);
+        }
+        int second=dp[n-1];
+        int first=dp[n-2];
+        
+        
+        
+        int min=(int)1e9;
+        if(n-2==1){
+            min=first;
+        }
+        
+        for(int i=n-3;i>=0;i--){
+            int val=Math.min(first,second);
+            int temp=first;
+            first=val+cost[i];
+            second=temp;
+            if(i==1||i==0){
+                min=Math.min(min,first);
+            }
+        }
+        
+        return min;
     }
     
-    public int solve(int index,int[] cost,int n){
+//     public int solve(int index,int[] cost,int n){
         
+// //         
+//         if(index>=n)return  0;
         
-        if(index>=n)return  0;
+//         if(dp[index]!=null)return dp[index];  
         
-        if(dp[index]!=null)return dp[index];  
+//       int oneStep=  solve(index+1,cost,n);
         
-      int oneStep=  solve(index+1,cost,n);
-        
-     int twoStep=   solve(index+2,cost,n);
-        int val= Math.min(oneStep,twoStep)+cost[index];
-     dp[index]=   val;
-        return val;
-    }
+//      int twoStep=   solve(index+2,cost,n);
+//         int val= Math.min(oneStep,twoStep)+cost[index];
+//      dp[index]=   val;
+//         return val;
+//     }
 }
