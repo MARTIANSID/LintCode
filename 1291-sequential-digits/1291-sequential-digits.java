c708ae9cc71f75e3cdd05e11757724bd078f2f03
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-01-23 14:41:15 +0530

    Time: 1 ms (41.30%), Space: 38.2 MB (30.98%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -1,27 +1,42 @@
 class Solution {
-    List<Integer> ans=new ArrayList<>();
+    // List<Integer> ans=new ArrayList<>();/
     public List<Integer> sequentialDigits(int low, int high) {
         
+        String s="123456789";
         
-        for(int i=1;i<=9;i++)
-         generate(low,high,0,i);
+        List<Integer> ans=new ArrayList<>(); 
         
         
-        Collections.sort(ans);
-            return ans;
+        for(int i=2;i<=9;i++){
+            for(int j=0;j+i<10;j++){
+                int num=Integer.parseInt(s.substring(j,j+i));
+                if(num>=low&&num<=high)ans.add(num);
+                else if(num>high) return ans;
+            }
+        }
+        
+        return ans;
+            
+        
+        //first add all the 2 digit numbers
+        //then 3 digit
+        //then 4 digit
+        //and so on
+        
+       
         //s="123456789"
         // 23456
     }
-    public void  generate(int low,int high,int current,int i){
+//     public void  generate(int low,int high,int current,int i){
         
-        if(current >= low && current <= high) {
-                ans.add(current);
-        }
+//         if(current >= low && current <= high) {
+//                 ans.add(current);
+//         }
         
-        if(current > high || i>9){
-            return;
-        }
+//         if(current > high || i>9){
+//             return;
+//         }
         
-            generate(low,high,current*10+i,i+1);
-    }
+//             generate(low,high,current*10+i,i+1);
+//     }
 }
