2b25cf0c54e87ccde87f5805cc6dd3059e32ef2c
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-01-26 16:28:33 +0530

    Added solution - LeetHub

---
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.java
@@ -0,0 +1,89 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            int n = Integer.parseInt(stt.nextToken());
+            int m = Integer.parseInt(stt.nextToken());
+            int k = Integer.parseInt(stt.nextToken());
+            int a[] = new int[(int)(n)];
+            int b[] = new int[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(inputLine1[i]);
+            }
+            
+            
+            Solution obj = new Solution();
+            System.out.println(obj.kthElement( a, b, n, m, k));
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
+        
+      //two pointer
+      int i=0,j=0;
+     
+     
+      while(i<n&&j<m&&k>1){
+        if(arr1[i]<arr2[j]){
+            i++;
+        }else{
+            j++;
+        }
+        k--;
+      }
+    if(k==1){
+          if(i>=n){
+          return arr2[j];
+      }else if(j>=m){
+          return arr1[i];
+      }
+
+      return Math.min(arr1[i],arr2[j]);
+    }else{
+       if(i>=n){
+           while(j<m&&k>1){
+               j++;
+               k--;
+           }
+           return arr2[j];
+       }else{
+            while(i<n&&k>1){
+               i++;
+               k--;
+           }
+           return arr1[i];
+       }
+        
+        
+    }     
+      
+    
+    }
+}
