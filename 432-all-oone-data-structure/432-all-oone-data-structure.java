83aed2cd68cbbd48b47c280027c078139bd697c9
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-01-28 00:15:35 +0530

    Time: 119 ms (25.27%), Space: 98.9 MB (5.30%) - LeetHub

--- 432-all-oone-data-structure/432-all-oone-data-structure.java
+++ 432-all-oone-data-structure/432-all-oone-data-structure.java
@@ -1,80 +1,180 @@
-class AllOne { 
+class AllOne {
     
-    TreeMap<Integer,HashMap<String,Integer>> tree;
-    HashMap<String,Integer> map;
+    HashMap<String,Integer> count;
+    HashMap<String,Node> keyToNode;
     
+    class Node{
+        String key;
+        Node prev;
+        Node next;
+        Node(String key){
+            this.key=key;
+        }
+    }
+    
+    
+    Node tail; //maximum
+    Node head; //minimum
+    
+    
+    private int size=0;
+        
+        
+    private void addFirst(Node node){
+        if(head==null){
+            head=node;
+            tail=node;
+           
+        }else{
+            head.prev=node;
+            node.next=head;
+            head=node;
+        }
+        size++;
+    }
+    
+    private void addLast(Node node){
+        if(head==null){
+            head=node;
+            tail=node;
+        }else{
+            tail.next=node;
+            node.prev=tail;
+            tail=node;
+        }
+        size++;
+    }
+    
+    private Node delete(Node node){
+        
+        if(size==0)return null;
+        
+        if(size==1){
+            head=null;
+            tail=null;
+        }else{
+            if(node.prev==null){
+                head=node.next;
+                node.next.prev=null;
+                node.next=null;
+            }else if(node.next==null){
+                tail=node.prev;
+                node.prev.next=null;
+                node.prev=null;
+            }else{
+                node.prev.next=node.next;
+                node.next.prev=node.prev;
+                node.prev=null;
+                node.next=null;
+            }
+        }
+        size--;
+        
+        return node;
+    }
+    
+    
+    private void swapInc(Node node){
+        Node next=node.next;
+        String temp=next.key;
+        next.key=node.key;
+        node.key=temp;
+        
+        keyToNode.put(node.key,node);
+        keyToNode.put(next.key,next);
+    }
+    
+    private  void swapDec(Node node){
+         Node prev=node.prev;
+        String temp=prev.key;
+        prev.key=node.key;
+        node.key=temp;
+        
+        keyToNode.put(node.key,node);
+        keyToNode.put(prev.key,prev);
+    }
+    
+
     public AllOne() {
-        this.tree=new TreeMap<>();
-        this.map=new HashMap<>();
+        count =new HashMap<>();
+        keyToNode=new HashMap<>();
     }
     
     public void inc(String key) {
-        if(map.containsKey(key)){
-           int oldCount= map.get(key);
+        if(count.containsKey(key)){
+           int newCount= count.get(key)+1;
+          int maxCount=  count.get(tail.key);
+             Node incNode= keyToNode.get(key);
+           
             
-            int newCount=oldCount+1;
             
-            map.put(key,newCount);
-            
-            tree.get(oldCount).remove(key);
-            if(tree.get(oldCount).size()==0)tree.remove(oldCount);
-            
-            if(tree.containsKey(newCount)){
-                tree.get(newCount).put(key,newCount);
-            }else{
-                HashMap<String,Integer> m=new HashMap<>();
-                m.put(key,newCount);
-                tree.put(newCount,m);
+            if(newCount>=maxCount){
+                delete(incNode);
+                addLast(incNode);
+                keyToNode.put(key,incNode);
+                
             }
+            
+           else if(incNode.next!=null){
+              Node next=  keyToNode.get(incNode.next.key);
+                
+                if(newCount>count.get(next.key)){
+                    swapInc(incNode);
+                }
+                
+            }
+            
+            
+            count.put(key,newCount);
+              
+            
         }else{
-                map.put(key,1);
-             if(tree.containsKey(1)){
-                tree.get(1).put(key,1);
-            }else{
-                HashMap<String,Integer> m=new HashMap<>();
-                m.put(key,1);
-                tree.put(1,m);
-            }
+            Node node=new Node(key);
+            addFirst(node);
+            count.put(key,1);
+            keyToNode.put(key,node);
         }
     }
     
     public void dec(String key) {
-         int oldCount= map.get(key);
-            
-            int newCount=oldCount-1;
-            if(newCount>0){
-                map.put(key,newCount);    
-            }else{
-                map.remove(key);
+        
+        int newCount= count.get(key)-1;
+        
+        if(newCount==0){
+           Node n= keyToNode.get(key);
+            delete(n);
+            keyToNode.remove(key);
+            count.remove(key);
+        }else{
+            Node n= keyToNode.get(key);
+             int minCount=  count.get(head.key);
+            if(newCount<=minCount){
+                delete(n);
+                addFirst(n);
+                keyToNode.put(key,n);
+                
             }
-            
-            tree.get(oldCount).remove(key);
-            if(tree.get(oldCount).size()==0)tree.remove(oldCount);
-            
-            if(newCount>0){
-                if(tree.containsKey(newCount)){
-                tree.get(newCount).put(key,newCount);
-            }else{
-                HashMap<String,Integer> m=new HashMap<>();
-                m.put(key,newCount);
-                tree.put(newCount,m);
-            }            
+           else if(n.prev!=null){ 
+               Node prev=  keyToNode.get(n.prev.key);
+               int cnt=count.get(prev.key);
+               if(cnt>newCount){
+                   swapDec(n);
+               }
+               
+           }
+            count.put(key,newCount);
         }
-
+        
     }
     
     public String getMaxKey() {
-        if(tree.size()==0)return "";
-       HashMap<String,Integer> m= tree.lastEntry().getValue();    
-        
-        return (String)(m.entrySet().iterator().next().getKey());
+        if(tail==null)return "";
+        return tail.key;
     }
     
     public String getMinKey() {
-         if(tree.size()==0)return "";
-       HashMap<String,Integer> m= tree.firstEntry().getValue();    
-       return (String)(m.entrySet().iterator().next().getKey());
-
+        if(head==null)return "";
+        return head.key;
     }
 }
 
