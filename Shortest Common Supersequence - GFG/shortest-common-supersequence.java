71f9d509ddd50b2f2c477fa53549f8d811e1f445
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-01-31 14:22:56 +0530

    Added solution - LeetHub

--- Shortest Common Supersequence - GFG/shortest-common-supersequence.java
+++ Shortest Common Supersequence - GFG/shortest-common-supersequence.java
@@ -38,30 +38,45 @@ class GFG {
 
 class Solution
 {
-       static Integer[][] dp;
+       static int[][] dp;
     public static int shortestCommonSupersequence(String X,String Y,int m,int n)
     {
         //Your code here
-          dp=new Integer[m][n];
+          dp=new int[m+1][n+1];
+          int max=-(int)1e9;
         
-        int value=solve(X,Y,0,0);
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                
+                if(X.charAt(i)==Y.charAt(j)){
+                    dp[i][j]=dp[i+1][j+1]+1;
+                   
+                }else{
+                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j+1]);
+                }
+                 max=Math.max(max,dp[i][j]);
+            }
+        }  
         
-        return X.length()+Y.length()-value;
+        
+        // int value=solve(X,Y,0,0);
+        
+        return X.length()+Y.length()-max;
     }
-    public static int solve(String a,String b,int i,int j){
+    // public static int solve(String a,String b,int i,int j){
         
-        if(i==a.length()||j==b.length())return 0;
+    //     if(i==a.length()||j==b.length())return 0;
         
-        if(dp[i][j]!=null)return dp[i][j];
+    //     if(dp[i][j]!=null)return dp[i][j];
         
-        if(a.charAt(i)==b.charAt(j)){
-            return dp[i][j]= solve(a,b,i+1,j+1)+1;
+    //     if(a.charAt(i)==b.charAt(j)){
+    //         return dp[i][j]= solve(a,b,i+1,j+1)+1;
             
-        }else{
+    //     }else{
           
-          int max=  Math.max(solve(a,b,i+1,j),solve(a,b,i,j+1));
-            return dp[i][j]= max;
-        }
+    //       int max=  Math.max(solve(a,b,i+1,j),solve(a,b,i,j+1));
+    //         return dp[i][j]= max;
+    //     }
         
-    }
+    // }
 }
