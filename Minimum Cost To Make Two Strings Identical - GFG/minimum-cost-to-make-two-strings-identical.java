4b14ee992a657e5ea633e644c351bddf59de3c71
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-02-01 11:03:42 +0530

    Added solution - LeetHub

--- Minimum Cost To Make Two Strings Identical - GFG/minimum-cost-to-make-two-strings-identical.java
+++ Minimum Cost To Make Two Strings Identical - GFG/minimum-cost-to-make-two-strings-identical.java
@@ -28,36 +28,69 @@ class GfG
 
 class Solution
 {
-    Integer[][] dp;
+    int[][] dp;
 	public int findMinCost(String X, String Y, int costX, int costY)
 	{
 		// Your code goes here
+		int n=X.length();
+		int m=Y.length();
 		
-		dp=new Integer[X.length()][Y.length()];
+		dp=new int[X.length()+1][Y.length()+1];
 		
-		return solve(X,Y,0,0,costX,costY);
+		for(int i=n;i>=0;i--){
+		    for(int j=m;j>=0;j--){
+		        
+		    if(i>=X.length()||j>=Y.length()){
+	        if(i<X.length()){
+	            
+	            dp[i][j]= costX*(X.length()-i);
+	            
+	        }else if(j<Y.length()){
+	            dp[i][j]= costY*(Y.length()-j); 
+	            
+	        }else{
+	            dp[i][j]= 0;
+	        }
+	        continue;
+	    }
+		        
+		        
+		   if(X.charAt(i)==Y.charAt(j)){
+	        dp[i][j]= dp[i+1][j+1];
+	    }else{
+	        dp[i][j]= Math.min(dp[i][j+1]+costY,dp[i+1][j]+costX);
+	    }
+		        
+		        
+		    }
+		}
+		
+		return dp[0][0];
+		
+		
+// 		return solve(X,Y,0,0,costX,costY);
 	}
 	
-	public int solve (String x,String y,int i,int j,int costX,int costY){
+// 	public int solve (String x,String y,int i,int j,int costX,int costY){
 	    
-	    if(i>=x.length()||j>=y.length()){
-	        if(i<x.length()){
+	   // if(i>=x.length()||j>=y.length()){
+	   //     if(i<x.length()){
 	            
-	            return costX*(x.length()-i);
-	        }else if(j<y.length()){
-	            return costY*(y.length()-j); 
-	        }else{
-	            return 0;
-	        }
-	    }
+	   //         return costX*(x.length()-i);
+	   //     }else if(j<y.length()){
+	   //         return costY*(y.length()-j); 
+	   //     }else{
+	   //         return 0;
+	   //     }
+	   // }
 	    
-	    if(dp[i][j]!=null)return dp[i][j];
+// 	    if(dp[i][j]!=null)return dp[i][j];
 	    
 	    
-	    if(x.charAt(i)==y.charAt(j)){
-	       return dp[i][j]= solve(x,y,i+1,j+1,costX,costY);
-	    }else{
-	       return dp[i][j]= Math.min(solve(x,y,i,j+1,costX,costY)+costY,solve(x,y,i+1,j,costX,costY)+costX);
-	    }
-	}
+// 	    if(x.charAt(i)==y.charAt(j)){
+// 	       return dp[i][j]= solve(x,y,i+1,j+1,costX,costY);
+// 	    }else{
+// 	       return dp[i][j]= Math.min(solve(x,y,i,j+1,costX,costY)+costY,solve(x,y,i+1,j,costX,costY)+costX);
+// 	    }
+// 	}
 }
