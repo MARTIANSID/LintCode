bb72cf6cae935763e5758de5911f5206e98fdf75
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-04-10 16:27:29 +0530

    Time: 15 ms (93.68%), Space: 49.5 MB (89.75%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.java
@@ -0,0 +1,74 @@
+class MyHashMap {
+
+    class Node {
+        int key;
+        int value;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    LinkedList<Node>[] buckets;
+    int size = 700;
+
+    public MyHashMap() {
+        this.buckets = new LinkedList[size];
+        for (int i = 0; i < size; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    private int getBucket(int key) {
+        
+        return key % size;
+    }
+
+    private int getIndexInBucket(int bucket, int key) {
+        LinkedList<Node> nodes = buckets[bucket];
+        int index = 0;
+        for (Node node : nodes) {
+            if (node.key == key) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        int bucket = getBucket(key);
+        int indexInBucket = getIndexInBucket(bucket, key);
+        if (indexInBucket == -1) {
+            buckets[bucket].add(new Node(key, value));
+        } else {
+            buckets[bucket].get(indexInBucket).value = value;
+        }
+    }
+
+    public int get(int key) {
+        int bucket = getBucket(key);
+        int indexInBucket = getIndexInBucket(bucket, key);
+        
+        if(indexInBucket == -1)return -1;
+        
+        return buckets[bucket].get(indexInBucket).value;
+    }
+
+    public void remove(int key) {
+        int bucket = getBucket(key);
+        int indexInBucket = getIndexInBucket(bucket, key);
+        
+        if(indexInBucket == -1)return;
+        
+        buckets[bucket].remove(indexInBucket);
+    }
+}
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
