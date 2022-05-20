9715116945f7bbee713462954b49821a8096978a
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-05-20 19:24:38 +0530

    Added solution - LeetHub

---
+++ K-th Largest Sum Contiguous Subarray - GFG/kth-largest-sum-contiguous-subarray.java
@@ -0,0 +1,79 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+        
+        return a;
+    }
+    
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+    
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N; 
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int K; 
+            K = Integer.parseInt(br.readLine());
+            
+            
+            int[] Arr = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            int res = obj.kthLargest(N, K, Arr);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public static int kthLargest(int n, int k, int[] arr) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<n ;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=arr[j];
+                if(pq.size()<k){
+                    pq.add(sum);
+                }else{
+                    pq.add(sum);
+                    pq.poll();
+                }
+            }
+        }
+        return pq.poll();
+    }
+}
+        
