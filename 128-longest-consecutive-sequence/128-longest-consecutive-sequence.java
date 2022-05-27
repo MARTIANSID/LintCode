929d4913869bfae77c64682e77bbf1c3b40cea6a
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-05-27 13:44:13 +0530

    Time: 48 ms (55.00%), Space: 71.5 MB (40.37%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,47 +1,34 @@
 class Solution {
-
-    public int findParent(HashMap<Integer, Integer> map, int u) {
-        if (map.get(u) == u) return u;
-
-        int x = findParent(map, map.get(u));
-        map.put(u, x);
-        return x;
-    }
-
     public int longestConsecutive(int[] nums) {
-        int n = nums.length;
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        HashSet<Integer> set = new HashSet<>();
-
-        for (int ele : nums) {
-            map.put(ele, ele);
-            set.add(ele);
+        int n=nums.length,max=0;
+        
+        HashMap<Integer,Integer> map=new HashMap<>();
+        
+        for(int ele:nums){
+            map.put(ele,1);
         }
-
-        int max = 0;
-
-        for (int ele : nums) {
-            if (set.contains(ele + 1)) {
-                int p1 = findParent(map, ele);
-                int p2 = findParent(map, ele + 1);
-                if (p1 != p2) {
-                    map.put(p1, p2);
-                }
+        
+        HashSet<Integer> set=new HashSet<>();
+        
+        
+        for(int ele:nums){
+            int count=0;
+            
+            if(!map.containsKey(ele) || set.contains(ele))continue;
+            
+    
+            int o=ele;
+            
+            while(map.containsKey(ele+1)){
+                count+=map.get(ele+1);
+                map.remove(ele+1);
+                ele=ele+1;
             }
+            
+            map.put(o,count+1);
+            max=Math.max(max,count+1);
+            set.add(o);
         }
-        HashSet<Integer> s = new HashSet<>();
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        for (int ele : nums) {
-            if (!s.contains(ele)) {
-                int p = findParent(map, ele);
-                freq.put(p, freq.getOrDefault(p, 0) + 1);
-                max = Math.max(max, freq.get(p));
-                s.add(ele);
-            }
-        }
-
         return max;
     }
 }
