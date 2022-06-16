deb17faec93ec426ae774fd303035557046e77c9
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-06-16 16:10:06 +0530

    Time: 1 ms (86.18%), Space: 42.2 MB (15.69%) - LeetHub

---
+++ 984-string-without-aaa-or-bbb/984-string-without-aaa-or-bbb.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String strWithout3a3b(int a, int b) {
+        boolean flag=a>b?false:true;
+        StringBuilder sb=new StringBuilder();
+        while(a > 0 && b > 0){
+            if(!flag){
+                a--;
+                sb.append('a');
+                if(a>b){
+                    sb.append('a');
+                    a--;
+                }
+                
+            }else{
+                 b--;
+                sb.append('b');
+                if(b>a){
+                    sb.append('b');
+                    b--;
+                }
+                
+            }
+            flag=!flag;
+        }
+        while(a-->0)sb.append('a');
+        while(b-->0)sb.append('b');
+        
+        return new String(sb);
+        
+    }
+}
