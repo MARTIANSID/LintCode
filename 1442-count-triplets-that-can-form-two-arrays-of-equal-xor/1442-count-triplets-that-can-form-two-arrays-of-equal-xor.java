5924bbbef643a109fbdab4e0e147a9ab2d7f683c
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-06-22 00:45:05 +0530

    Time: 2 ms (79.43%), Space: 41.1 MB (73.14%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.java
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.java
@@ -1,19 +1,36 @@
 class Solution {
+    
+    /*
+    logic - > (i,j,k)
+    xor from (i,j-1) is equal lets say a
+    xor from (j,k) is equal to lets say b
+    now both of these xor are equal that means a^b=0
+    that also means xor of number from (i,k) is zero
+    
+    now lets we found a found two points some i and k for which xor from (i,k) is zero
+    in btw there will some points lets say  i,j1,j2,j3,j4,k points j1,j2,j3,j4 will be our possible answer
+    why?
+    consider xor from (i,j1) =a
+    and xor from (j3,k) = b
+    a will be equal to b because (a^b)=0 (when two values gives xor as zero that means a=b)
+    
+    */
+    
     public int countTriplets(int[] arr) {
         int n=arr.length;
         
-        int count=0;
+        int ans=0;
         
         for(int i=0;i<n;i++){
-            int xor=arr[i];
+            int ele=arr[i];
+            int xor=ele;
             for(int j=i+1;j<n;j++){
                 xor^=arr[j];
-                if(xor==0 ){
-                    count+=(j-i);
+                if(xor==0){
+                    ans+=(j-i);
                 }
             }
         }
-        
-        return count;
+        return ans;
     }
 }
