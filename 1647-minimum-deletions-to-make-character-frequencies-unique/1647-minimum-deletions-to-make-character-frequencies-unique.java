7f635238a4f26cfe4f7fe73caf88a3b5f14f89c5
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-06-28 11:13:18 +0530

    Time: 26 ms (33.90%), Space: 53.6 MB (79.00%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,31 +1,22 @@
 class Solution {
-
     public int minDeletions(String s) {
-        int n = s.length();
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        int[] charCount = new int[26];
+        
+        for(char ch : s.toCharArray()){
+            charCount[ ch - 'a'] ++;
         }
-        List<Integer> freq = new ArrayList<>();
-
-        for (Map.Entry mapElement : map.entrySet()) {
-            freq.add((int)mapElement.getValue());
-        }
-        Collections.sort(freq);
         
-        int size=freq.size(),deletions=0;
+        HashSet<Integer> set = new HashSet<>();
+        int deletion = 0;
         
-        
-        for(int i=size-2;i>=0;i--){
-            if(freq.get(i)>=freq.get(i+1)){
-                int val=(freq.get(i)-(freq.get(i+1)==0?1:freq.get(i+1)))+1;
-                deletions+=val;
-                freq.set(i,freq.get(i)-val);
+        for(int val : charCount){
+            while(val !=0 && set.contains(val)){
+                val--;
+                deletion++;
             }
+            set.add(val);          
         }
-        return deletions;
+       
+        return deletion;
     }
 }
