487c7e30ad432d2d2895c587a3215ce049a709f9
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-07-07 01:22:06 +0530

    Time: 83 ms (5.15%), Space: 72.5 MB (5.15%) - LeetHub

--- 1764-form-array-by-concatenating-subarrays-of-another-array/1764-form-array-by-concatenating-subarrays-of-another-array.java
+++ 1764-form-array-by-concatenating-subarrays-of-another-array/1764-form-array-by-concatenating-subarrays-of-another-array.java
@@ -1,15 +1,16 @@
 class Solution {
-    LinkedList<Integer> nos = new LinkedList<>();
-
-    public boolean kmp(int[] group) {
+   
+    int index=0;
+    public boolean kmp(int[] group,int[] nums) {
         LinkedList<Integer> list = new LinkedList<>();
-        for (int ele : nos) {
-            list.add(ele);
+        for (int i=index;i<nums.length;i++) {
+            list.add(nums[i]);
         }
         list.addFirst(-(int) 1e9);
         for (int i = group.length - 1; i >= 0; i--) {
             list.addFirst(group[i]);
         }
+        
 
         int[] lps = new int[list.size()];
         int i = 1, len = 0;
@@ -17,10 +18,7 @@ class Solution {
             if (Integer.compare(list.get(i), list.get(len)) == 0) {
                 len++;
                 if (len == group.length) {
-                    int index = i - len - 1;
-                    for (int j = 0; j <= index; j++) {
-                        nos.removeFirst();
-                    }
+                    index=(i-len-1)+1;
                     return true;
                 }
                 lps[i] = len;
@@ -37,12 +35,10 @@ class Solution {
     }
 
     public boolean canChoose(int[][] groups, int[] nums) {
-        for (int ele : nums) {
-            nos.add(ele);
-        }
+      
 
         for (int[] g : groups) {
-            if (!kmp(g)) return false;
+            if (!kmp(g,nums)) return false;
         }
         return true;
     }
