fe13c112b5bf368215b068ce05411743a0cb3b95
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-07-15 21:34:33 +0530

    Added solution - LeetHub

---
+++ Intersection Point in Y Shapped Linked Lists - GFG/intersection-point-in-y-shapped-linked-lists.java
@@ -0,0 +1,165 @@
+// { Driver Code Starts
+import java.util.*;
+
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+	
+ class LinkedList_Intersection
+{
+    Node head = null;  
+	Node tail = null;
+
+public void addToTheLast(Node node) 
+{
+
+  if (head == null) {
+   head = node;
+   tail = head;
+  } else {
+   //Node temp = head;
+   //while (temp.next != null)
+    //temp = temp.next;
+
+   //temp.next = node;
+   tail.next=node;
+   tail = node;
+  }
+}
+
+  /* Function to print linked list */
+    void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+	 
+ 
+     /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+       
+         
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 
+		 while(t>0)
+         {
+			int n1 = sc.nextInt();
+			int n2 = sc.nextInt();
+			int n3 = sc.nextInt();
+			LinkedList_Intersection llist1 = new LinkedList_Intersection();
+		    LinkedList_Intersection llist2 = new LinkedList_Intersection();
+			LinkedList_Intersection llist3 = new LinkedList_Intersection();
+			
+				int a1=sc.nextInt();
+				Node head1= new Node(a1);
+				Node tail1= head1;
+				
+				for (int i = 1; i < n1; i++) 
+				{
+					int a = sc.nextInt(); 
+					tail1.next = (new Node(a));
+					tail1= tail1.next;
+				}
+			
+			
+				int b1=sc.nextInt();
+				Node head2 = new Node(b1);
+				Node tail2 = head2;
+				for (int i = 1; i < n2; i++) 
+				{
+					int b = sc.nextInt();  
+					tail2.next = (new Node(b));
+					tail2= tail2.next;
+				}
+				if(n3>0){
+				int c1=sc.nextInt();
+				Node head3= new Node(c1);
+				tail1.next = head3;
+				tail2.next = head3;
+				Node tail3=head3;
+				for (int i = 1; i < n3; i++) 
+				{
+					int c = sc.nextInt();   
+					tail3.next = (new Node(c));
+					tail3= tail3.next;
+				}
+				}
+				Intersect obj = new Intersect();
+				System.out.println(obj.intersectPoint(head1, head2));
+			t--;			
+         }
+    }
+}
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+ Linked List class
+class LinkedList
+{
+    Node head;  // head of list
+}*/
+
+class Intersect
+{
+    public int getLength(Node head){
+        int count=0;
+        while(head!=null){
+            count++;
+            head=head.next;
+        }
+        return count;
+    }
+    //Function to find intersection point in Y shaped Linked Lists.
+	int intersectPoint(Node head1, Node head2)
+	{
+	    int l1=getLength(head1);
+	    int l2=getLength(head2);
+	    int extra=Math.abs(l1-l2);
+	    
+	    
+	   // System.out.println(l1+" "+l2);
+	    
+	    if(l1>l2){
+	        while(extra-->0){
+	            head1=head1.next;
+	        }
+	    }else{
+	        while(extra-->0){
+	            head2=head2.next;
+	        }
+	    }
+	  
+	    
+	    while(head1!=head2){
+	        head1=head1.next;
+	        head2=head2.next;
+	    }
+	    return head1.data;
+        
+	}
+}
+
+
