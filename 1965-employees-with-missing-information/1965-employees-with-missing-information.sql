e194321e8ab456db61c378ef588e42a4a6b39b26
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-07-31 10:40:56 +0530

    Time: 625 ms (50.70%), Space: 0B (100.00%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,13 @@
+
+select e.employee_id
+from Employees as e
+left join Salaries as s
+on e.employee_id = s.employee_id
+where s.salary is null
+union
+select s.employee_id
+from Salaries as s
+left join Employees as e
+on s.employee_id = e.employee_id
+where e.name is null
+order by employee_id asc
