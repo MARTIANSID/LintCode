dd098515fb3324ca02c9e088c0f72c671cb0225d
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2024-01-10 00:36:45 +0530

    Time: 130 ms (76.67%), Space: 44.7 MB (79.72%) - LeetHub

--- 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
@@ -3,33 +3,30 @@ class Solution {
         
         int n = nums.size();
         
-     
-        
-        HashMap<Integer,Integer> map = new HashMap<>();  // sum : freq
+      int[] dp = new int[Math.max(target+1,n+1)];
         
         for(int i=0;i<n;i++){
-            HashMap<Integer,Integer> temp=new HashMap<>();
+            int[] temp=new int[Math.max(target+1,n+1)];
             for(int j=0;j<=target;j++){
                 int sum = j;
                 int valReq = sum - nums.get(i);
         
-                if(map.containsKey(valReq)){
-                    int len = map.get(valReq);
-                    temp.put(sum,Math.max(map.getOrDefault(sum,0),len+1));
+                if(valReq >=0 && dp[valReq] > 0){
+                    int len = dp[valReq];
+                    temp[sum]=Math.max(dp[valReq],len+1);
                 } 
             }
             
-            for(int key : temp.keySet()){
-                map.put(key,Math.max(temp.get(key),map.getOrDefault(key,0)));
+            for(int j=0;j<=target;j++){
+                dp[j]=Math.max(temp[j],dp[j]);
             }
             
-            map.put(nums.get(i),Math.max(map.getOrDefault(nums.get(i),0),1));
+            if(nums.get(i)<=target)
+            dp[nums.get(i)] = Math.max(dp[nums.get(i)],1);
             
         }
         
-
-        
-        return map.containsKey(target) ? map.get(target) : -1;
+        return dp[target] > 0 ? dp[target] : -1;
     }
 }
 
