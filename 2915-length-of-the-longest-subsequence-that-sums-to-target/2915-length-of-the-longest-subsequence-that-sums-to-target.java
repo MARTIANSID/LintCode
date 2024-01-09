8d6557413c5e5f3e69522f4775d9295a471e68bc
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2024-01-10 00:27:16 +0530

    Time: 1601 ms (5.27%), Space: 45.5 MB (77.22%) - LeetHub

--- 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
@@ -1,26 +1,35 @@
 class Solution {
-    Integer[][] dp;
-
     public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
-        dp = new Integer[nums.size() + 1][target + 1];
-
-        int ans = solve(nums, target, 0);
         
-        return ans < 0 ? -1 : ans;
-    }
+        int n = nums.size();
+        
+     
+        
+        HashMap<Integer,Integer> map = new HashMap<>();  // sum : freq
+        
+        for(int i=0;i<n;i++){
+            HashMap<Integer,Integer> temp=new HashMap<>();
+            for(int j=0;j<=target;j++){
+                int sum = j;
+                int valReq = sum - nums.get(i);
+        
+                if(map.containsKey(valReq)){
+                    int len = map.get(valReq);
+                    temp.put(sum,Math.max(map.getOrDefault(sum,0),len+1));
+                } 
+            }
+            
+            for(int key : temp.keySet()){
+                map.put(key,Math.max(temp.get(key),map.getOrDefault(key,0)));
+            }
+            
+            map.put(nums.get(i),Math.max(map.getOrDefault(nums.get(i),0),1));
+            
+        }
+        
 
-    public int solve(List<Integer> nums, int target, int index) {
-        if (target == 0) return 0;
-
-        if (index == nums.size()) return -(int) 1e9;
-
-        if (dp[index][target] != null) return dp[index][target];
-
-        int max = -(int)1e9;
-
-        if (target - nums.get(index) >= 0) max = solve(nums, target - nums.get(index), index + 1) + 1;
-        max = Math.max(max, solve(nums, target, index + 1));
-
-        return dp[index][target] = max;
+        
+        return map.containsKey(target) ? map.get(target) : -1;
     }
 }
+
