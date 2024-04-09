10b8707f3f6a0dd75f3daed61fb99783e5aa1ec0
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2024-04-09 13:07:37 +0530

    Time: 43 ms (35.70%), Space: 16.5 MB (95.21%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        
+        countOfOnes = 0
+        countOfZeros = 0
+        
+        for ele in students:
+            if ele == 1:
+                countOfOnes+=1
+            else:
+                countOfZeros+=1
+        
+        for ele in sandwiches:
+            if ele == 1:
+                if countOfOnes > 0:
+                    countOfOnes-=1
+                else:
+                    break
+            else:
+                if countOfZeros > 0:
+                    countOfZeros-=1
+                else:
+                    break
+        return countOfOnes+countOfZeros
+        
