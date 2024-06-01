78db7587d41e3db9ffd6c7c0f6047feba17479ff
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2024-06-01 13:51:17 +0530

    Time: 77 ms (19.92%), Space: 61.2 MB (39.42%) - LeetHub

---
+++ 3153-sum-of-digit-differences-of-all-pairs/3153-sum-of-digit-differences-of-all-pairs.java
@@ -0,0 +1,54 @@
+class Solution {
+
+
+    public long sumDigitDifferences(int[] nums) {
+        HashMap<Integer, Integer>[] digitDiff = new HashMap[10];
+
+        for (int i = 0; i < 10; i++) {
+            digitDiff[i] = new HashMap<>();
+        }
+
+        for (int ele : nums) {
+            int index = 0;
+            while (ele > 0) {
+                int digit = ele % 10;
+                digitDiff[index].put(digit, digitDiff[index].getOrDefault(digit, 0) + 1);
+                ele /= 10;
+                index++;
+            }
+        }
+        
+       
+
+        long ans = 0;
+
+        for (HashMap<Integer, Integer> map : digitDiff) {
+            Set<Integer> keys = map.keySet();
+
+            if (keys.size() == 0 || keys.size() == 1) continue;
+
+            int n = keys.size();
+
+            long temp = 0, totalFreq = 0;
+
+            for (int key : keys) {
+                int freq = map.get(key);
+                totalFreq += freq;
+
+                if (freq >= 2) {
+                    temp += ((freq)*(freq-1)) / (2);
+                }
+            }          
+
+            long total = ((totalFreq)*(totalFreq-1)) / 2;
+
+            ans += (total - temp);
+        }
+        return ans;
+    }
+}
+
+// [1,1,1,4]
+// 4c2 = 4!/2!*2! = 6
+// 3c2 = 3
+// 6-3 = 3
